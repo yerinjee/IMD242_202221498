53b162d501c8ce3b31009e78ed1f219919ea919a
yerinjee <162898931+yerinjee@users.noreply.github.com>
2024-11-04 19:02:33 +0900

    9주차강의3

--- w9/first_p5_js/sketch.js
+++ w9/first_p5_js/sketch.js
@@ -1,8 +1,29 @@
+let tileNumX = 16;
+let tileNumY = 12;
+
 function setup() {
   createCanvas(640, 480);
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 100);
+  background(100, 100, 255);
+  noStroke();
+  fill(230, 255, 210);
+  // for (let column = 0; column < width; column += 40) {
+  //   for (let row = 0; row < height; row += 40) {
+  //     let x = 20 + column;
+  //     let y = 20 + row;
+  //     let diametar = 30;
+  //     circle(x, y, diametar);
+  //   }
+  // }
+  for (let row = 0; row < tileNumY; row++) {
+    for (let column = 0; column < tileNumX; column++) {
+      let tileW = width / tileNumX;
+      let tileH = height / tileNumY;
+      let x = tileW * 0.5 + column * tileW;
+      let y = tileH * 0.5 + row * tileH;
+      ellipse(x, y, tileW, tileH);
+    }
+  }
 }
